6653855fd33c6ca7b442b3db9e8a4b4ab2cfc5c0
rohan030101 <janraorohan2@gmail.com>
2024-02-02 23:25:00 +0530

    Added hashing password and serving file to user using express js

--- main.js
+++ main.js
@@ -1,19 +1,31 @@
-const express = require('express');
-const bodyParser = require('body-parser');
+const express = require("express");
+const bcrypt = require("bcrypt");
+const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
+const salt = 10;
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use(express.static('public'));
-app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
 
-app.post('/login', (req,res)=>{
-    const {username,password} = req.body;
-    res.json({sucess:true,message:"Login Sucessful"});
-    }
-)
+  bcrypt.genSalt(salt, function (err, salt) {
+    bcrypt.hash(password, salt, function (err, hash) {
+      console.log(hash);
+      console.log(`The UserID and Password is ${username} and ${hash}`);
+    });
+  });
 
+  //   res.send("Login Successfull ");
+  res.sendFile(__dirname + "/public/dashboard.html");
+});
 
-app.listen(port, ()=>{
-    console.log(`Server Listening on port http://localhost:${port}`);
-}) 
+app.listen(port, () => {
+  console.log(`Server Listening on port http://localhost:${port}`);
+});
