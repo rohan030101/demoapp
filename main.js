ea00e63352e7b135d04a438f3762c6378cffda61
rohan030101 <janraorohan2@gmail.com>
2024-02-03 20:46:29 +0530

    added models and routes in projects for better readebility

--- main.js
+++ main.js
@@ -12,24 +12,7 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
-app.post("/login", (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
 
-  bcrypt.genSalt(salt, function (err, salt) {
-    bcrypt.hash(password, salt, function (err, hash) {
-      console.log(hash);
-      console.log(`The UserID and Password is ${username} and ${hash}`);
-      const token = jwt.sign({username},"soc",{
-        expiresIn:"1m"
-      })
-      console.log(token);
-    });
-  });
-
-  //   res.send("Login Successfull ");
-  res.sendFile(__dirname + "/public/dashboard.html");
-});
 
 app.listen(port, () => {
   console.log(`Server Listening on port http://localhost:${port}`);
