66ccdac78c4d12a5a7d0f9e4e6ebe8fde0744441
rohan030101 <janrarohan2@gmail.com>
2024-02-07 22:26:06 +0530

    added funtionality to upload user to databse to a specific collections

--- Models/UserModel.js
+++ Models/UserModel.js
@@ -1,8 +1,26 @@
 const mongoose = require("mongoose");
+const bcrypt = require("bcrypt");
+const salt = 10;
 
 const userSchema = new mongoose.Schema({
   username: { type: String, unique: true, required: true },
   password: { type: String, required: true },
 });
+
+
+// userSchema.pre('save',  function(next){
+//   if(this.password && this.isModified('password')) {
+//     bcrypt.hash(this.password,10,(err,hashed)=>{
+//       if(err) return next(err);
+//       this.password = hashed;
+//       console.log(hashed);
+//       next()
+//     });
+//    } else{
+//       next()
+//     }
+// });s
+// let User = mongoose.model('User', userSchema)
+// module.exports = User;
 
 module.exports = mongoose.model("User", userSchema);
--- Routes/adduser.js
+++ Routes/adduser.js
@@ -1,25 +1,39 @@
-const express = require('express');
+const express = require("express");
 const { MongoClient } = require("mongodb");
-const router = express.Router(); 
-const dotenv = require('dotenv')
+const router = express.Router();
+const dotenv = require("dotenv");
+const UserModel = require("../Models/UserModel");
 dotenv.config();
-
+const bcrypt = require("bcrypt");
 const client = new MongoClient(process.env.DB_URl);
-const database  = client.db('esocietyapp');
+const database = client.db("esocietyapp");
+const userCollection = database.collection("users");
 
 router.get("/", async (req, res) => {
-    const users = database.collection('users');
-    let results = await users.find({});
-    res.status(200).json(results);
+  const users = database.collection("users");
+  let results = await users.find({}).toArray((err, result) => {
+    if (err) throw err;
+    console.log(result);
+  });
+  console.log(results);
+  res.status(200).json(results);
 });
 
-
-router.post('/adduser',async(req,res)=>{
-   let username = req.body.username;
-   let password = req.body.password;    
-   console.log(username, password);
-   res.sendStatus(200);
+router.post("/adduser", async (req, res) => {
+  let user = UserModel(req.body);
+  bcrypt.hash(user.password, 10, (err, hash) => {
+    user.password = hash;
+    console.log(user.password);
+  });
+  res.sendStatus(200);
+  let result = await userCollection
+    .insertOne(user)
+    .then(() => {
+      res.status(200);
+    })
+    .catch((e) => {
+      console.log(e);
+      res.status(500);
+    });
 });
-
-
 module.exports = router;
